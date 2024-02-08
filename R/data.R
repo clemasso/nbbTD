b5007eedc97fd5073aa196d4140e7915875e0cfb
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-02-08 13:55:37 +0100

    some linting (spaces, new lines, ->)

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
 #' table_rw data set
-#' 
+#'
 #' Simulated data used to create table with critical value for the
 #' automatic selection of the model for the forecasting of the annual BI ratio
-#' 
+#'
 #' @format A data.table with 11 columns including series length and the
 #'         critical value for various quantiles
 #' @source simulated data
-#' 
+#'
 "table_rw"
